560e57e103739590ae392cbd8ac4605cef497ae2
m-najeeb <muhammadnajeeb2022@gmail.com>
2025-03-18 11:38:13 +0500

    category CRUD operations added

---
+++ app/api/(dashboard)/categories/route.ts
@@ -0,0 +1,97 @@
+import connect from "@/lib/db";
+import User from "@/lib/models/users";
+import Category from "@/lib/models/category";
+import { NextResponse } from "next/server";
+import { Types } from "mongoose";
+
+export const GET = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(
+        JSON.stringify({ message: "User not found in the database" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    const categories = await Category.find({
+      user: new Types.ObjectId(userId),
+    });
+
+    return new NextResponse(JSON.stringify(categories), {
+      status: 200,
+    });
+  } catch (error: unknown) {
+    return new NextResponse(
+      `Error in fetching category: ${
+        error instanceof Error ? error.message : String(error)
+      }`,
+      {
+        status: 500,
+      }
+    );
+  }
+};
+
+export const POST = async (request: Request) => {
+  try {
+    const { searchParams } = new URL(request.url);
+    const userId = searchParams.get("userId");
+
+    const { title } = await request.json();
+
+    if (!userId || !Types.ObjectId.isValid(userId)) {
+      return new NextResponse(
+        JSON.stringify({ message: "Invalid or missing userId" }),
+        {
+          status: 400,
+        }
+      );
+    }
+
+    await connect();
+
+    const user = await User.findById(userId);
+    if (!user) {
+      return new NextResponse(JSON.stringify({ message: "User not found" }), {
+        status: 404,
+      });
+    }
+    const newCategory = new Category({
+      title,
+      user: new Types.ObjectId(userId),
+    });
+
+    await newCategory.save();
+
+    return new NextResponse(
+      JSON.stringify({ message: "Category is created", category: newCategory }),
+      { status: 200 }
+    );
+  } catch (error: unknown) {
+    return new NextResponse(
+      `Error in creating category: ${
+        error instanceof Error ? error.message : String(error)
+      }`,
+      {
+        status: 500,
+      }
+    );
+  }
+};
